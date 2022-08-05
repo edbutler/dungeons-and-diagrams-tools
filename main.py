981493b25836e31bbf11807757d28497e50dab30
Eric Butler <edbutler@users.noreply.github.com>
2022-08-05 12:45:26 -0700

    add comments

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ def test(p:Puzzle):
         print_solution(p, soln)
 
 p = Puzzle(
+    # the numer of filled blocks for each row
     rows=[2,1,4,4,2,5,2,4],
+    # the numer of filled blocks for each column
     cols=[1,3,3,3,3,5,3,3],
+    # the 0-indexed x,y location of each monster in the grid
     monsters=[(7,0),(7,4),(6,5),(7,6)],
+    # the 0-indexed x,y location of each chest in the grid
     chests=[(0,0)])
 
 test(p)
