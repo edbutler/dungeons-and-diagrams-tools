9a1f75234d4983bb28881ba03729b2a3597c95bc
Eric Butler <edbutler@users.noreply.github.com>
2022-08-05 22:31:26 -0700

    comment typo fixes

--- rules.py
+++ rules.py
@@ -116,7 +116,7 @@ def add_constraints(solver:Solver, puzzle:SymbolicPuzzle|ConcretePuzzle, soln:Sy
             for y in range(BoardSize)
             for (a,b) in [(x,y+1), (x+1,y)]
             if a < BoardSize and b < BoardSize]
-    # duplicate the entire edge list to represent both directions since the constraints are for directed graphs
+    # Duplicate the entire edge list to represent both directions since the constraints are for directed graphs.
     edges = edges + [(b,a) for (a,b) in edges]
     assert_connected(solver, nodes, edges)
 
@@ -129,14 +129,14 @@ def assert_connected(
 
     bits = math.ceil(math.log2(len(potential_nodes)))
 
-    # we choose one node to be the source, and distances represents the minimal distance
+    # We choose one node to be the source, and distances represents the minimal distance
     # to each node from the source. used_edge indicates, for each edge, whether it's part
     # of any such paths.
 
     distances = [z3.BitVec(f'distance-{i}', bits) for i in range(len(potential_nodes))]
     used_edge = [z3.Bool(f'used-edge-{i}') for i in range(len(edges))]
 
-    # exactly one node is the source node
+    # Exactly one node is the source node.
     is_zero_distance = [0 == d for d in distances]
     solver.add(z3.Or(*is_zero_distance))
     for (b1, b2) in itertools.combinations(is_zero_distance, 2):
@@ -153,31 +153,31 @@ def assert_connected(
         incoming_edges = [x for x, e in enumerate(edges) if e[1] == i]
         outgoing_edges = [x for x, e in enumerate(edges) if e[0] == i]
 
-        # the source node must be in the graph and have no used incoming_edges
+        # The source node must be in the graph and have no used incoming edges.
         solver.add(
             z3.Implies(
                 is_source,
                 z3.And(is_node, z3.Not(any_used_edges(incoming_edges)))))
 
-        # if this node and a neighbor are both in the graph, then this node must
-        # be at most one further away than the neighbor (meaning it's connected)
+        # If this node and a neighbor are both in the graph, then this node must
+        # be at most one further away than the neighbor (meaning it's connected).
         for e in incoming_edges:
             o = edges[e][0]
             solver.add(
                 z3.Implies(z3.And(is_node, potential_nodes[o]),
                 distances[i] <= distances[o]+1))
 
-        # if this node is in the graph, then it must be connected by an edge.
-        # conversely, if this node isn't in the graph, none of the edges can be used
-        # (including the outgoing edges)
+        # If this node is in the graph, then it must be connected by an edge.
+        # Conversely, if this node isn't in the graph, none of the edges can be used
+        # (including the outgoing edges).
         solver.add(
             z3.Implies(
                 z3.And(is_node, z3.Not(is_source)),
                 any_used_edges(incoming_edges)))
-        # not the converse is stronger (incluces outgoing_edges), so we can't simplify to an equivalence
+        # Note that the converse is stronger (incluces outgoing_edges), so we can't simplify to an equivalence.
         solver.add(z3.Implies(any_used_edges(incoming_edges+outgoing_edges), is_node))
 
-        # if connected by an edge, then this node is exactly one distance away from the neighbor
+        # If connected by an edge, then this node is exactly one distance away from the neighbor.
         for e in incoming_edges:
             o = edges[e][0]
             solver.add(z3.Implies(used_edge[e], distances[i] == distances[o] + 1))
